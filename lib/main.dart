0cc9c7e62f09805943af16b52e91347f890bc5b7
S Mendoza <samuelmendozal06@gmail.com>
2026-09-19 13:09:48 -0500

    CreateloginClass

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:proyecto_movil_navegacion/Layout/login.dart';
 
 void main() => runApp(const MercadoApp());
 
@@ -8,10 +9,10 @@ class MercadoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mercado Campesino',
-      home: const Scaffold(
-        body: Center(child: Text('Mercado Campesino')),
-      ),
+      home: Login()
+      ,
     );
   }
 }
